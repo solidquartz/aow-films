b37c4f1a97ef1b5d959d2b5ea90f0fcd922e1a9a
Aky Mac Donald <akymacd@gmail.com>
2022-11-04 15:13:46 -0400

    Added HTML and content for Films

--- pages/films.tsx
+++ pages/films.tsx
@@ -4,8 +4,23 @@ type Props = {}
 
 function Films({}: Props) {
   return (
-    <div>films</div>
-  )
+    <div>
+      <div>
+        <h2>Films</h2>
+      </div>
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/RU-mt9uHe6A"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default Films
