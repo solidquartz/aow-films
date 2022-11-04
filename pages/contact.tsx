5371d3797930ca3905b84ccda4c4c0f7744ee975
Aky Mac Donald <akymacd@gmail.com>
2022-11-04 14:44:14 -0400

    Added HTML and content for Contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function Contact({}: Props) {
   return (
-    <div>contact</div>
-  )
+    <div>
+      <div>
+        <h2>Contact</h2>
+      </div>
+      <div>
+        <h3>Andrew Wolinsky</h3>
+
+        <p>Email: awolinsky@gmail.com</p>
+
+        <p>Telephone: +1 (416) 525 FILM (3456) </p>
+
+        <p>Instagram: https://www.instagram.com/awolie01/</p>
+
+        <p>Facebook: https://www.facebook.com/awolinsky</p>
+
+        <p>LinkedIn: https://www.linkedin.com/in/andrewwolinsky/</p>
+
+        <p>Vimeo: https://vimeo.com/user64502745</p>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
